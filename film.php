a4e2998c19eace2e09cb949608bf74c45a644c43
wheeemyse <talla.m.seck@gmail.com>
2018-07-22 22:30:02 +0200

    push du dimanche

---
+++ film.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/stylefilm.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <div>
+      <h2 class="detailtext">Black Panther</h2>
+      <div class="film1">
+        <img class="imgfilm" src="./img/film/wakanda.jpeg" alt="Wakanda">
+        <p class="detailtext synopsis">Synopsis: <br>Après les événements qui se sont déroulés dans Captain America : Civil War, T’Challa revient chez lui prendre sa place sur le trône du Wakanda, une nation africaine technologiquement très avancée. Mais lorsqu’un vieil ennemi resurgit, le courage de T’Challa est mis à rude épreuve, aussi bien en tant que souverain qu’en tant que Black Panther. Il se retrouve entraîné dans un conflit qui menace non seulement le destin du Wakanda, mais celui du monde entier…</p>
+      </div>
+    </div>
+    <div class="detailfilm">
+      <figure class="detailacteur">
+        <img src="./img/film/realisateur.jpg" alt="realisateur">
+        <p class="detailtext">Réalisateur</p>
+        <p class="detailtext">Ryan Coogler</p>
+      </figure>
+      <figure class="detailacteur">
+        <img src="./img/film/acteur1.jpg" alt="acteur1">
+        <p class="detailtext">Acteur</p>
+        <p class="detailtext">Chadwick Boseman</p>
+      </figure>
+      <figure  class="detailacteur">
+        <img src="./img/film/acteur2.jpg" alt="acteur2">
+        <p class="detailtext">Acteur</p>
+        <p class="detailtext">Michael B. Jordan</p>
+      </figure>
+    </div>
+  </body>
+</html>
